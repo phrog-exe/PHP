19786b632c130cde4a6e58258e0633cdb004fad5
Weronika <91962861+s24404@users.noreply.github.com>
2022-06-08 15:12:08 +0200

    change of aesthetic

--- Translator.php
+++ Translator.php
@@ -2,24 +2,24 @@
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <head>
-     <link rel="stylesheet"
-               href=
-               "https://unpkg.com/purecss@1.0.0/build/pure-min.css"
-               integrity=
-               "sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w"
-               crossorigin="anonymous" />
+    <link rel="stylesheet"
+          href=
+          "https://unpkg.com/purecss@1.0.0/build/pure-min.css"
+          integrity=
+          "sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w"
+          crossorigin="anonymous" />
 
 <style>
-    @import url("http://fonts.googleapis.com/css?family=Cinzel+Decorative:700");
+
     body {
 
 
-        background: linear-gradient(-45deg, #87E879, #BF90E8,#4568dc, #b06ab3);
+        background: linear-gradient(-45deg, #bc5090, #ff6361,#003f5c, #b06ab3);
         background-size: 400% 400%;
         animation: gradient 15s ease infinite;
         height: 100vh;
 
-        font-family: "Cinzel Decorative", serif;
+        font-family: 'Courier New', monospace;
         font-size: 112.5%;
         text-align: center;
         font-weight: 700;
@@ -46,7 +46,7 @@ Buttons
 
     .button1 {
 
-        font-family: lato,sans-serif;
+        font-family: 'Courier New', monospace;
         font-weight: bold;
         font-size: 1em;
         letter-spacing: 0.1em;
@@ -62,7 +62,7 @@ Buttons
     }
 
 .button2 {
-    font-family: lato,sans-serif;
+    font-family: 'Courier New', monospace;
     font-weight: bold;
     font-size: 1em;
     letter-spacing: 0.1em;
@@ -78,7 +78,7 @@ Buttons
 }
 
 .button3 {
-    font-family: lato,sans-serif;
+    font-family: 'Courier New', monospace;
     font-weight: bold;
     font-size: 1em;
     letter-spacing: 0.1em;
@@ -129,15 +129,21 @@ Buttons
         box-shadow: 0 12px 24px rgba(128, 128, 128, 0.1);
     }
 
-.button_save {
-    margin-left: 650px;
 
+
+    /*  button download*/
+
+.button_save {
+margin-left: 1200px;
 }
+
 
 /*
 text areas
  */
      .area1 {
+         outline: none;
+
          font-family: lato,sans-serif;
          font-weight: bold;
          font-size: 1em;
@@ -145,13 +151,15 @@ text areas
          text-decoration: none;
         background-color: #393e46;
          color: white;
-         padding: 10px 40px 10px 40px;
+         padding: 60px 80px 60px 80px;
          position: relative;
          border: 3px solid #ffffff;
          border-radius: 25px;
         resize: none;
     }
      .area2 {
+         outline: none;
+
          font-family: lato,sans-serif;
          font-weight: bold;
          font-size: 1em;
@@ -159,18 +167,26 @@ text areas
          text-decoration: none;
          background-color: #393e46;
          color: white;
-         padding: 10px 40px 10px 40px;
+         padding: 60px 80px 60px 80px;
          position: relative;
          border: 3px solid #ffffff;
          border-radius: 25px;
          resize: none;
      }
+     .label1 {
+         margin-right: 1050px;
+         color: white;
 
+     }
+
+     .h1 {
+         color: white;
+     }
 </style>
 </head>
 <body>
 
-<h1>CIPHER ENCODER / DECODER</h1><br><br>
+<h1 class="h1">CIPHER ENCODER / DECODER</h1><br><br>
 
 <div>
     <form method="get" action="">
@@ -179,10 +195,12 @@ text areas
         <input class="button2" type ="button" value="Caesar cipher" onclick="">&nbsp&nbsp
         <input class="button3" type ="button" value="Affine cipher" onclick=""><br><br><br><br>
 
-        <label for="text1">Enter text you want to encode: </label><br><br><br>
+        <label class="label1" for="text1">Enter text you want to encode: </label><br><br>
 
-        <textarea class="area1" name="text1" id="text1" placeholder="Hello World" rows="10" cols="30"></textarea>&nbsp &nbsp &nbsp
-        <textarea readonly="readonly" class="area2" name="text2" id="text2" rows="10" cols="30" placeholder=".... . .-.. .-.. --- / .-- --- .-. .-.. -.."></textarea><br>
+        <textarea class="area1" name="text1" id="text1" placeholder="Hello World" rows="10" cols="50"></textarea>&nbsp &nbsp &nbsp
+        <textarea readonly="readonly" class="area2" name="text2" id="text2" rows="10" cols="50" placeholder=".... . .-.. .-.. --- / .-- --- .-. .-.. -.."></textarea><br>
+
+
         <button class="button_save" onclick=saveTextAsFile(text2.value,'download.txt')>Download</button><br><br>
 
          <input class="button_sub" type="submit" value="Submit">
